e24c1f25900dfcd3a5579d5cbf08b8b47ce40854
Vitalsonchik123 <Vitaliir2006@rambler.ru>
2026-04-22 21:06:56 +0300

    push lab_4

--- backend/src/index.js
+++ backend/src/index.js
@@ -79,13 +79,12 @@ function saveData() {
     fs.writeFileSync(dataPath, JSON.stringify(stocks, null, 2));
 }
 
-// CORS для разработки (можно оставить для совместимости, но при статике не нужен)
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+//app.use((req, res, next) => {
+//    res.header('Access-Control-Allow-Origin', '*');
+//    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+//    res.header('Access-Control-Allow-Headers', 'Content-Type');
+//    next();
+//});
 
 app.use(express.json());
 
